c382b2488cc4d0c044553a7a3501e101070458d4
Bipin Singh <bipinkainbox@gmail.com>
2024-09-11 19:09:43 +0530

    [287] [Time Beats: 64.19%] [Memory Beats: 93.89%] - LeetPush

--- 287-Find-the-Duplicate-Number.cpp
+++ 287-Find-the-Duplicate-Number.cpp
@@ -19,3 +19,14 @@ public:
         return slow;
     }
 };
+
+    // unordered_map<int, int> mp;
+    //     int i;
+
+    //     for(i=0; i<nums.size(); i++){
+    //         mp[nums[i]]++;
+    //     }
+    //     for(auto it:mp){
+    //         if(it.second > 1) return it.first;
+    //     }
+    //     return -1;
