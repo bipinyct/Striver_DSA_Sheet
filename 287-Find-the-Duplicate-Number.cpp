25cf3c8db078f1780f8bb26c68363b33523fd5d7
Bipin Singh <bipinkainbox@gmail.com>
2024-09-11 19:07:30 +0530

    [287] [Time Beats: 52.68%] [Memory Beats: 84.52%] - LeetPush

--- 287-Find-the-Duplicate-Number.cpp
+++ 287-Find-the-Duplicate-Number.cpp
@@ -1,16 +1,21 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        unordered_map<int, int> mp;
-        int i;
+        int slow = nums[0];
+        int fast = nums[0];
 
-        for(i=0; i<nums.size(); i++){
-            mp[nums[i]]++;
-        }
-        for(auto it:mp){
-            if(it.second > 1) return it.first;
-        }
-        return -1;
+        
+        do{
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }while(slow!= fast);
 
+        fast = nums[0];
+
+        while(slow!= fast){
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
     }
 };
