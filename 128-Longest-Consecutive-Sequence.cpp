360c97c22ff75ee0d5f97030fa23d6adac19d754
Bipin Singh <bipinkainbox@gmail.com>
2024-09-15 16:18:40 +0530

    [128] [Time Beats: 45.13%] [Memory Beats: 33.97%] - LeetPush

--- 128-Longest-Consecutive-Sequence.cpp
+++ 128-Longest-Consecutive-Sequence.cpp
@@ -2,21 +2,44 @@ class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
         if(nums.size() == 0) return 0;
-        sort(nums.begin(), nums.end());
-        int last_int = INT_MIN;
         int longest =1;
-        int cnt = 0;
+        int cnt =0;
+        unordered_set<int> set;
+
         for(int i=0; i<nums.size(); i++){
-            if(nums[i] - 1 == last_int){
-                cnt++;
-                last_int = nums[i];
-            }
-            else if (last_int != nums[i]){
+            set.insert(nums[i]);
+        }
+
+        for(auto it : set){
+            if(set.find(it-1) == set.end()){
                 cnt = 1;
-                last_int = nums[i];
+                int x = it;
+                while(set.find(x+1) != set.end()){
+                    x = x + 1;
+                    cnt++;
+                }
+                longest = max(longest, cnt);
             }
-            longest = max(longest, cnt);
         }
         return longest;
     }
 };
+
+
+// if(nums.size() == 0) return 0;
+//         sort(nums.begin(), nums.end());
+//         int last_int = INT_MIN;
+//         int longest =1;
+//         int cnt = 0;
+//         for(int i=0; i<nums.size(); i++){
+//             if(nums[i] - 1 == last_int){
+//                 cnt++;
+//                 last_int = nums[i];
+//             }
+//             else if (last_int != nums[i]){
+//                 cnt = 1;
+//                 last_int = nums[i];
+//             }
+//             longest = max(longest, cnt);
+//         }
+//         return longest;
