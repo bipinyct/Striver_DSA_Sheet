fa167f35d2f2e808eb936bf0e70104c28a530f85
Bipin Singh <bipinkainbox@gmail.com>
2024-12-03 01:23:14 +0530

    Time: 0 ms (100%), Space: 18.1 MB (38.51%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,38 +1,39 @@
 class Solution {
 public:
-
-    vector<int> topologicalsort(vector<vector<int>>& adj, int n, vector<int>& indegree){
+    vector<int> topologicalsort(vector<vector<int>>& adj, int n, vector<int>& indegree) {
         queue<int> q;
-
         vector<int> result;
         int count = 0;
 
-        for(int i=0; i<n; i++){
-            if(indegree[i] ==0){
+        // Push nodes with 0 indegree into the queue
+        for (int i = 0; i < n; i++) {
+            if (indegree[i] == 0) {
+                q.push(i);
                 result.push_back(i);
                 count++;
-                q.push(i);
-
             }
         }
 
-        while(!q.empty()){
+        while (!q.empty()) {
             int u = q.front();
             q.pop();
 
-            for(int &v : adj[u]){
+            for (int &v : adj[u]) {
                 indegree[v]--;
-                if(indegree[v] == 0){
+                if (indegree[v] == 0) {
+                    q.push(v);
                     result.push_back(v);
                     count++;
-                    q.push(v);
                 }
             }
         }
-        if(count == n){
+
+        // If we processed all nodes, return the result
+        if (count == n) {
             return result;
         }
 
+        // Otherwise, return an empty vector indicating a cycle
         return {};
     }
 
@@ -40,13 +41,15 @@ public:
         vector<int> indegree(numCourses, 0);
         vector<vector<int>> adj(numCourses);
 
-        for(auto &vec : prerequisites){
+        // Build the adjacency list and calculate indegrees
+        for (auto &vec : prerequisites) {
             int a = vec[0];
             int b = vec[1];
             adj[b].push_back(a);
             indegree[a]++;
         }
 
+        // Perform topological sorting
         return topologicalsort(adj, numCourses, indegree);
     }
 };
